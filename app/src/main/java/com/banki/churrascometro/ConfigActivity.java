cd0e870070fbe0f17d45b562f844e1b63606dcd5
André Luiz Banki <banki@altoqi.com.br>
2015-09-07 20:31:24 -0300

    Botões Ajuda e Config

---
+++ app/src/main/java/com/banki/churrascometro/ConfigActivity.java
@@ -0,0 +1,17 @@
+package com.banki.churrascometro;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by andre on 07/09/2015.
+ */
+public class ConfigActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+    }
+
+}
--- app/src/main/java/com/banki/churrascometro/MainActivity.java
+++ app/src/main/java/com/banki/churrascometro/MainActivity.java
@@ -1,9 +1,14 @@
 package com.banki.churrascometro;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import static android.widget.Toast.LENGTH_LONG;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        criaBtnAjuda();
+        criaBtnConfig();
+    }
+
+    private void criaBtnAjuda() {
+        Button btnAjuda = (Button)findViewById(R.id.btnAjuda);
+        btnAjuda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast t = Toast.makeText(
+                        MainActivity.this,
+                        R.string.ajuda,
+                        LENGTH_LONG);
+                t.show();
+            }
+        });
+    }
+
+    private void criaBtnConfig() {
+        Button btnConfig = (Button)findViewById(R.id.btnConfig);
+        btnConfig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ConfigActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
