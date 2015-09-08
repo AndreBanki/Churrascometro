648a52553dae01bfce28db79cc5428ec617c3fa3
André Luiz Banki <banki@altoqi.com.br>
2015-09-07 22:20:06 -0300

    Protótipo da tela Config

--- app/src/main/java/com/banki/churrascometro/ConfigActivity.java
+++ app/src/main/java/com/banki/churrascometro/ConfigActivity.java
@@ -1,17 +1,37 @@
 package com.banki.churrascometro;
 
+import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
-/**
- * Created by andre on 07/09/2015.
- */
-public class ConfigActivity extends AppCompatActivity {
+public class ConfigActivity extends TabActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
-    }
 
+        TabHost tabHost = getTabHost();
+
+        TabSpec tab1 = tabHost.newTabSpec("Homens");
+        tab1.setIndicator("Homens");
+        Intent intent1 = new Intent(this, ConfigTabActivity.class);
+        tab1.setContent(intent1);
+
+        TabSpec tab2 = tabHost.newTabSpec("Mulheres");
+        tab2.setIndicator("Mulheres");
+        Intent intent2 = new Intent(this, ConfigTabActivity.class);
+        tab2.setContent(intent2);
+
+        TabSpec tab3 = tabHost.newTabSpec("Crianças");
+        tab3.setIndicator("Crianças");
+        Intent intent3 = new Intent(this, ConfigTabActivity.class);
+        tab3.setContent(intent3);
+
+        tabHost.addTab(tab1);
+        tabHost.addTab(tab2);
+        tabHost.addTab(tab3);
+    }
 }
---
+++ app/src/main/java/com/banki/churrascometro/ConfigTabActivity.java
@@ -0,0 +1,16 @@
+package com.banki.churrascometro;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
+
+public class ConfigTabActivity extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.data_config);
+    }
+}
